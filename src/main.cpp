1a27421a761ddfe8c27c982c50ac01be6dc346c6
LSka <ls@inventati.org>
2018-12-27 17:13:11 +0100

    regression to standar openGL declaration

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 
 //--------------------------------------------------------------
 int main(){
-	//ofSetupOpenGL(1440, 900, OF_FULLSCREEN);
+	ofSetupOpenGL(1440, 900, OF_FULLSCREEN);
     
-    ofGLESWindowSettings settings;
+    /*ofGLESWindowSettings settings;
     settings.setGLESVersion(2);
     settings.setSize(1440, 900);
     settings.windowMode = OF_FULLSCREEN;
-    ofCreateWindow(settings);
+    ofCreateWindow(settings);*/
     
 	ofRunApp(new ofApp()); // start the app
 }
